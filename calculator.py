040635ef300fc42d152aa5ff3ff1c7d978d5e4d2
Harshith Reddy <harshithreddy293@gmail.com>
2025-05-22 15:12:16 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 a = int(input("Give one Number: "))
-b = int(input("Give another number: "))
 c = input("Give the operation to be performed(+,-,*,/): ")
+b = int(input("Give another number: "))
 if   c == "+":
         print(f"The sum is:{a+b}")
 elif c == "-":
