269b34e8010f68de7716b029a7d63a7c8543452e
Harshith Reddy <harshithreddy293@gmail.com>
2025-05-22 15:21:10 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,15 @@
+#Finding factorial for given input
+try:
+    num = int(input("Enter a non-negative integer: "))
+
+    if num < 0:
+            print("Factorial is not defined for negative numbers.")
+    elif num == 0:
+            print(f"The factorial of 0 is: 1")
+    else:
+        factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i  
+        print(f"The factorial of {num} is: {factorial}")
+except ValueError:
+        print("Invalid input. Please enter an integer.")
